bf1f1fb3da3350ec5ba02e404859350062fbaef0
adam moore <adamaoc@gmail.com>
2015-07-21 07:13:11 -0500

    updating ember --- all is breaking...

--- app/routes/products.js
+++ app/routes/products.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	// pull in the model here in the route
 	model: function() {
-		return this.store.find('product', {
+		return this.store.query('product', {
 			limitToLast: 9
 		});
 	}
